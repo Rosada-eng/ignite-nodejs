0380547de9bc83c3bbdfa916d6c54bdf706aa875
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2024-09-09 18:55:40 -0300

    feat(CreateNewTask): implement POST route to create a new task

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import http from 'node:http';
+import { TasksDatabase } from './infraestructure/database/database.js';
+
+const hostname = '0.0.0.0';
+const port = 3333;
+
+const server = http.createServer(async (request, response) => {
+
+    const tasksDatabase = new TasksDatabase();
+
+    const { url, method } = request;
+
+    const buffers = [];
+
+    for await (const chunk of request) {
+        buffers.push(chunk);
+    }
+
+    try {
+        request.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        request.body = null;
+    }
+
+    if (method === 'POST' && url === '/tasks') {
+        const { title, description } = request.body;
+
+        const task = tasksDatabase.create(title, description);
+        
+        response.statusCode = 201;
+        response.setHeader('Content-Type', 'application/json');
+        return response.end(JSON.stringify(task));
+
+    }
+
+
+
+
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    }
+);
