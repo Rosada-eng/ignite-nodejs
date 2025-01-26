716a0154e3317686fbbe896b6abfad4b234f01ed
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2025-01-26 00:15:44 +0000

    edit(.env): add PORT

--- 03-SOLID/src/config/env.ts
+++ 03-SOLID/src/config/env.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod'
+import 'dotenv/config'
 
 export const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'prod']).default('dev'),
     DATABASE_URL: z.string(),
+    PORT: z.coerce.number().default(3333),
 })
 
 export const env = envSchema.parse(process.env)
