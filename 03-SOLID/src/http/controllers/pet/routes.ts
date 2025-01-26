45782d52b184fabb97e8e06777da0961db95ab1c
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2025-01-26 02:04:39 +0000

    feat(pet-routes): add update and adopt routes

--- 03-SOLID/src/http/controllers/pet/routes.ts
+++ 03-SOLID/src/http/controllers/pet/routes.ts
@@ -2,16 +2,20 @@ import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { remove } from './delete'
+import { update } from './update'
+import { adopt } from './adopt'
 
-export async function authenticatedPetRoutes(app: FastifyInstance) {
+export async function protectedPetRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT)
 
     app.post('/adoption', register)
     app.delete('/adoption', remove)
+    app.put('/adoption', update)
+    app.put('/adoption/adopt', adopt)
 }
 
 export async function petRoutes(app: FastifyInstance) {
-    app.register(authenticatedPetRoutes, {
+    app.register(protectedPetRoutes, {
         prefix: '/pet',
     })
 }
