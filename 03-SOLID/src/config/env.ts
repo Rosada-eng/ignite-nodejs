defb9ae7fdf9dd97a24b4fafd9c2d657af60a1f7
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2025-01-27 00:17:40 +0000

    chore(vitest): add integration test config

--- 03-SOLID/src/config/env.ts
+++ 03-SOLID/src/config/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import 'dotenv/config'
 
 export const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'prod']).default('dev'),
+    NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
     DATABASE_URL: z.string(),
     PORT: z.coerce.number().default(3333),
 })
